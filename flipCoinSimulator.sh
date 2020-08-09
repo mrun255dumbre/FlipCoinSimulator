977af1058ce16bf46fd01e076487df5bd79d79f0
mrun255dumbre <mrun255dumbre@gmail.com>
2020-08-09 11:31:25 +0530

    second branch

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -8,3 +8,29 @@ then
 else
         echo "It's Tail"
 fi
+
+headCnt=0;
+tailCnt=0;
+read -p "number of flips" n
+for (( i=1; i<=$n; i++ ))
+do
+        flip=$((RANDOM%2))
+        if [ $flip -eq 1 ]
+        then
+                headCnt=$(($headCnt + 1))
+        else
+                tailCnt=$(($tailCnt + 1))
+        fi
+done
+
+if [[ $headCnt -gt $tailCnt ]]
+then
+        wonBy=$(($headCnt-$tailCnt))
+        echo "Head is won by " $wonBy
+elif [[ $tailCnt -gt $headCnt ]]
+then
+        wonBy=$(($tailCnt-$headCnt))
+        echo "Tail is won by " $wonBy
+else
+        echo "Tie"
+fi
